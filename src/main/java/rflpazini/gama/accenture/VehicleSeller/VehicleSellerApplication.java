b1e5624fb64bcb3b2377c1f649d52a5141793844
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-10-19 22:03:30 -0300

    Initial commit

---
+++ src/main/java/rflpazini/gama/accenture/VehicleSeller/VehicleSellerApplication.java
@@ -0,0 +1,13 @@
+package rflpazini.gama.accenture.VehicleSeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSellerApplication.class, args);
+	}
+
+}
